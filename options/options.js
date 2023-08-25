80f2c6b7eb654afa6fe8584bcb40b8ac1f3fd7ae
Santosh Purbey <purbey.santosh@hotmail.com>
2023-08-25 19:24:47 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "gentle-dew-34",
+  url: "https://gentle-dew-34.botics.co",
+  api: "https://gentle-dew-34.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
